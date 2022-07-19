4bbeca8c3de84e76ed2a9a02470b9d688eb45c71
Whale Mo <ncwhale@gmail.com>
2022-07-19 11:13:03 +0900

    Update route to FastAPI style.

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,14 +1,14 @@
 __version__ = '0.1.0'
 
-from quart import Quart, request, jsonify, render_template
+# from quart import Quart, request, jsonify, render_template
+
 from datetime import datetime
 
-app = Quart(__name__)
+# app = Quart(__name__)
+from fastapi import FastAPI, Request, Body
 
-def run() -> None:
-    app.run()
+app = FastAPI()
 
 @app.post("/echo")
-async def echo():
-    data = await request.get_json()
+async def echo(data: dict = Body()):
     return {"input": data, "timestamp": str(datetime.now())}
