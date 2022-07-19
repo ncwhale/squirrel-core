c275de73a1ff0dad4318a06f3122c292d0828514
Whale Mo <ncwhale@gmail.com>
2022-07-19 21:20:03 +0900

    Add collector router.

--- squirrel_core/app.py
+++ squirrel_core/app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from .api import router as api_routes
+from .api import router as api_router
+from .collector import router as collector_router
 
 app = FastAPI()
 
-app.include_router(api_routes, prefix="/api")
+app.include_router(api_router, prefix="/api")
+app.include_router(collector_router, prefix="/collector")
--- squirrel_core/collector/__init__.py
+++ squirrel_core/collector/__init__.py
@@ -4,3 +4,4 @@ from .hookspec import CollectorHookspec
 # Register the hookspec for Collector
 pm.add_hookspecs(CollectorHookspec)
 
+from .router import router
