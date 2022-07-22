353fada2ec4d79e01b5d9647a3f44c13bef95d05
Whale Mo <ncwhale@gmail.com>
2022-07-22 18:10:11 +0900

    Link route to workflow.

--- squirrel_core/collector/router.py
+++ squirrel_core/collector/router.py
@@ -1,3 +1,17 @@
+from datetime import datetime
 from fastapi import APIRouter
+from .schema import GrabRequest
+from .flow import AddGrabRequest
 
 router = APIRouter()
+
+@router.get("/")
+async def input_view():
+    # It's a simple form for manual input grab request.
+    # TODO: Add a form for manual input grab request.
+    return "Hello World!"
+
+@router.post("/")
+async def add_task(task: GrabRequest):
+    result = await AddGrabRequest(task)
+    return {"result": result, "timestamp": datetime.now()}
