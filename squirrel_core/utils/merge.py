c1e83af59f466ab1a73f92a50c2cfabca26021ce
Whale Mo <ncwhale@gmail.com>
2022-07-24 00:34:30 +0900

    Add dict_deep_merge and test.

---
+++ squirrel_core/utils/merge.py
@@ -0,0 +1,29 @@
+
+
+def dict_deep_merge(a: dict, b: dict) -> dict:
+    """
+    Merge two dictionaries.
+    """
+    for key in b:
+        if key in a:
+            if isinstance(a[key], dict) and isinstance(b[key], dict):
+                # Recursive for dict merge.
+                dict_deep_merge(a[key], b[key])
+            elif isinstance(a[key], dict) and not isinstance(b[key], dict):
+                # dict is fist here. None dict values cannot merge to dict keys.
+                raise ValueError("Cannot merge a dictionary with other types.")
+            elif a[key] == b[key]:
+                # Same value, do nothing.
+                pass
+            elif isinstance(a[key], list) and isinstance(b[key], list):
+                # Merge list.
+                a[key].extend(b[key])
+            elif isinstance(a[key], set) and not isinstance(b[key], set):
+                # Merge set.
+                a[key] = a[key] | b[key]
+            else:
+                # Merge other types.
+                a[key] = b[key]
+        else:
+            a[key] = b[key]
+    return a
