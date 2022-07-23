e7f35ae59d4a6eab7b3ede12df8f3a6fbffaa7ef
Whale Mo <ncwhale@gmail.com>
2022-07-24 00:07:11 +0900

    add templates for project.

--- squirrel_core/collector/router.py
+++ squirrel_core/collector/router.py
@@ -1,16 +1,20 @@
 from datetime import datetime
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+
 from ..hooks import pm
+from ..templates import templates
 from .schema import GrabRequest
 from .flow import AddGrabRequest
 
 router = APIRouter()
 
-@router.get("/")
-async def input_view():
+
+@router.get("/", response_class=HTMLResponse)
+async def input_view(request: Request):
     # It's a simple form for manual input grab request.
     # TODO: Add a form for manual input grab request.
-    return "Hello World!"
+    return templates.TemplateResponse("collector/input.html", {"request": request})
 
 @router.post("/")
 async def add_task(task: GrabRequest):
