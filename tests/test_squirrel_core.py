61f9689da3927f3fe8c23764365f164e6f757ea8
Whale Mo <ncwhale@gmail.com>
2022-07-26 07:32:37 +0900

    Add assert comments.

--- tests/test_squirrel_core.py
+++ tests/test_squirrel_core.py
@@ -2,5 +2,5 @@ from squirrel_core import __version__ as core_version
 from squirrel_core.api import __version__ as api_version
 
 def test_version():
-    assert core_version == '0.1.0'
-    assert api_version == '0.1.0'
+    assert core_version == '0.1.0' , "Core version should be 0.1.0."
+    assert api_version == '0.1.0' , "API version should be 0.1.0."
--- tests/utils/test_merge.py
+++ tests/utils/test_merge.py
@@ -8,7 +8,7 @@ def test_dict_deep_merge():
     a = {'a': {'b': {'c': {'d': 1, 'l': [1, 2, 3]}}}}
     b = {'a': {'b': {'c': {'e': 2, 'l': [4, 5, 6]}}}}
     c = dict_deep_merge(dict(a), b)
-    assert c == {'a': {'b': {'c': {'d': 1, 'e': 2, 'l': [1, 2, 3, 4, 5, 6]}}}}
+    assert c == {'a': {'b': {'c': {'d': 1, 'e': 2, 'l': [1, 2, 3, 4, 5, 6]}}}} , "Everything should be merged."
 
     # Test 2: Merge two dictionaries with some key is not dict.
     a = {'a': {'b': {'c': {'d': 1, 'l': {'x': 1, 'y': 2, 'z': 3}}}}}
