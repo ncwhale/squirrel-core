c0bcdca8f65f02d0a3dd89b360dc19a6e42b447f
Whale Mo <ncwhale@gmail.com>
2022-10-10 14:53:24 +0900

    move schema.py.

--- squirrel_core/schema.py
+++ squirrel_core/schema.py
@@ -1,3 +1,4 @@
+from imp import _FileLike
 from pydantic import BaseModel
 from typing import Optional
 
@@ -8,3 +9,7 @@ class GrabRequest(BaseModel):
     body: Optional[dict] = None
     cookies: Optional[dict] = None
     metadata: Optional[dict] = None
+
+class Resource(BaseModel):
+    metadata: Optional[dict] = None
+    blob: Optional[_FileLike] = None
